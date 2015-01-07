f0782a17b13281893c4623009055a3a4c76fcb5d
Thomas Fetcke <Thomas.Fetcke@googlemail.com>
2015-01-07 08:30:33 +0100

    Created Plot 2 script and image.

---
+++ plot2.R
@@ -0,0 +1,15 @@
+# Exploratory Data Analysis Course Project 1: Plot 2
+
+# Read the data.
+source("read.R")
+
+# Set the language for the week day labels on the date axis.
+Sys.setlocale(locale="en_US")
+
+# Create the PNG file as required.
+png(filename="plot2.png", width = 480, height = 480)
+
+# Plot a line with the given axis labels.
+with(data, plot(DateTime, Global_active_power, type="l",
+     xlab="", ylab="Global Active Power (kilowatts)"))
+dev.off()
